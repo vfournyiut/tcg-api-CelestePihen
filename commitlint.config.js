f6dc880ddcffb11e64fcede6d5815681cd8e0b40
Céleste PIHEN <celcraftpro@gmail.com>
2026-02-13 10:45:21 +0100

    chore(git-config): add commitlint and husky config

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'perf',
+        'ci',
+        'build',
+        'revert',
+      ],
+    ],
+    'subject-case': [2, 'never', ['upper-case', 'pascal-case']],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+  },
+}
